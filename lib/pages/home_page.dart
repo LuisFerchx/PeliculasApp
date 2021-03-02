9a62a44ddb2c398cd2cb22b1430a9c5a9d2cdefb
Luis Guerrero <luixg22@gmail.com>
2021-03-02 00:37:47 -0500

    consumir la api y mostrar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
+  final peliculasProvider = new PeliculasProvider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +26,26 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
-    return CardSwiper(peliculas: [1,2,3,4,5]);
+    return FutureBuilder(
+      future: peliculasProvider.getEstrenos(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiper(
+              peliculas: snapshot.data); //retorna unicamente cuando existe data en el snapshot
+        } else {
+          return Container(
+            height: 400.0,
+            child: Center(
+              child: CircularProgressIndicator()
+              ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          );
+        }
+      },
+    );
+
+    //return CardSwiper(peliculas: [1,2,3,4,5]);
   }
 }
